034173834d941173bf55538828b1c99d65242b71
mwiszenko <wiszenkom@gmail.com>
2021-05-07 19:09:11 +0200

    Add unit tests for requires

--- src/requires.pl
+++ src/requires.pl
@@ -4,3 +4,13 @@
 requires(move(What, From/on(What, From), On), [clear(What), clear(On)]).
 
 requires(move(What/on(What, From), From, _), [clear(What/on(What, From))]).
+
+:- begin_tests(requires).
+
+test(1, Conds == [clear(a), clear(c)]) :-
+    requires(move(a, b/on(a, b), c), Conds).
+
+test(2, Conds == [clear(a/on(a, b))]) :-
+    requires(move(a/on(a, b), b, c), Conds).
+
+:- end_tests(requires).
