836be30fa8a621e3216ce4b2a3e93c266b497e4e
mwiszenko <wiszenkom@gmail.com>
2021-05-18 22:48:40 +0200

    Implement tests for inst_action, conds_achieved, inst

--- src/inst_action.pl
+++ src/inst_action.pl
@@ -34,3 +34,52 @@ inst(A, _, A).
 
 inst(A/B, State, A) :-
     goal_achieved(B, State).
+
+% test inst_action
+
+:- begin_tests(inst_action).
+
+test(1, [nondet]) :-
+    inst_action(move(a, b, c), on(a, c), [on(a, b), clear(d)], X),
+    X == move(a, b, c).
+
+test(2, [fail]) :-
+    inst_action(move(a, b, c), on(a, c), [on(a, d), clear(c)], _).
+
+test(3, [nondet]) :-
+    inst_action(move(a/on(a, b), b, c/clear(c)), clear(b), [on(a, b), clear(c)], X),
+    X == move(a, b, c).
+
+test(4, [fail]) :-
+    inst_action(move(a, b, c), clear(b), [on(a, d), clear(d)], _).
+
+:- end_tests(inst_action).
+
+% test conds_achieved
+
+:- begin_tests(conds_achieved).
+
+test(1, [nondet]) :-
+    conds_achieved(a/on(a, b), c, [on(a, b), clear(c)]).
+
+test(2, [nondet]) :-
+    conds_achieved(a, c/clear(c), [on(a, b), clear(c)]).
+
+test(3, [fail]) :-
+    conds_achieved(a/on(a, b), c, [on(a, d), clear(c)]).
+
+:- end_tests(conds_achieved).
+
+% test inst
+
+:- begin_tests(inst).
+
+test(1, [nondet]) :-
+    inst(a/on(a, b), [on(a, b), clear(c)], X),
+    X == a.
+
+test(2) :-
+    inst(a, [on(a, b), clear(c)], X),
+    X == a.
+
+:- end_tests(inst).
