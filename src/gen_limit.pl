4e655eee25082ed3c468ed9d2fd49d83ccec467d
mwiszenko <wiszenkom@gmail.com>
2021-05-18 22:13:29 +0200

    Implement tests for gen_limit

--- src/gen_limit.pl
+++ src/gen_limit.pl
@@ -7,3 +7,19 @@ gen_limit(Curr, Max, Next) :-
     Curr < Max,
     NewCurr is Curr + 1,
     gen_limit(NewCurr, Max, Next).
+
+% test gen_limit
+
+:- begin_tests(gen_limit).
+
+test(1, [nondet]) :-
+    gen_limit(10, 50, 10).
+
+test(2, [nondet]) :-
+    gen_limit(0, 5, X),
+    X == 1.
+
+test(3, [fail]) :-
+    gen_limit(6, 5, _).
+
+:- end_tests(gen_limit).
