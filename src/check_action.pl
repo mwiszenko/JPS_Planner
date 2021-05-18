00b64f4a8ea2959b260373a7527665ed9a9d7c41
mwiszenko <wiszenkom@gmail.com>
2021-05-18 22:19:48 +0200

    Implement tests for check_action

--- src/check_action.pl
+++ src/check_action.pl
@@ -5,3 +5,21 @@
 check_action(move(What, From, On), AchievedGoals) :-
     not(goal_achieved(on(What, From), AchievedGoals)),
     not(goal_achieved(clear(On), AchievedGoals)).
+
+% test check_action
+
+:- begin_tests(check_action).
+
+test(1) :-
+    check_action(move(a, b, c), [on(a, c)]).
+
+test(2, [fail]) :-
+    check_action(move(a, b, c), [on(a, b)]).
+
+test(3) :-
+    check_action(move(a, b/on(a, b), c), [on(a, c), clear(p1)]).
+
+test(4, [fail]) :-
+    check_action(move(a, b/on(a, b), c), [on(a, b)]).
+
+:- end_tests(check_action).
