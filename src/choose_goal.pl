01f6f92754523d9e812f3f83a459dbe1fc90316e
mwiszenko <wiszenkom@gmail.com>
2021-05-18 22:28:05 +0200

    Implement tests for choose_goal

--- src/choose_goal.pl
+++ src/choose_goal.pl
@@ -8,3 +8,17 @@ choose_goal(Goal, [Goal|RestGoals], RestGoals, State) :-
 	
 choose_goal(Goal, [X|Goals], [X|RestGoals], State) :-
     choose_goal(Goal, Goals, RestGoals, State).
+
+% test choose_goal
+
+:- begin_tests(choose_goal).
+
+test(1, [nondet]) :-
+    choose_goal(X, [on(a, b), clear(c)], Y, []),
+    X == on(a, b), Y == [clear(c)].
+
+test(2, [nondet]) :-
+    choose_goal(X, [on(a, b), clear(c)], Y, [on(a, b)]),
+    X == clear(c), Y = [on(a, b)].
+
+:- end_tests(choose_goal).
