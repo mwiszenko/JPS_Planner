8f3038dae67226da97bca6dc426eef92d5243160
mwiszenko <wiszenkom@gmail.com>
2021-05-18 21:59:53 +0200

    Implement tests for achieves

--- src/achieves.pl
+++ src/achieves.pl
@@ -6,3 +6,17 @@
 achieves(on(What, On), move(What, From/(on(What, From)), On)).
 
 achieves(clear(On), move(What/on(What, On), On, _)).
+
+% test achieves
+
+:- begin_tests(achieves).
+
+test(1) :-
+    achieves(on(a, b), move(X, c/on(Y, c), Z)),
+    X == a, Y == a, Z == b.
+
+test(2) :-
+    achieves(clear(a), move(b/on(b, X), Y, c)),
+    X == a, Y == a.
+
+:- end_tests(achieves).
