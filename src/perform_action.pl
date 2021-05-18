9b4711d91f4e9cbba359816dc4702c81e29668c2
mwiszenko <wiszenkom@gmail.com>
2021-05-18 22:04:12 +0200

    Implement tests for perform_action

--- src/perform_action.pl
+++ src/perform_action.pl
@@ -10,3 +10,13 @@ perform_action(State1, move(What, From, On), [on(What, On), clear(From) | State2
     mem(on(What, From), State1),
     del(on(What, From), State1, StateTemp),
     del(clear(On), StateTemp, State2).
+
+% test perform_action
+
+:- begin_tests(perform_action).
+
+test(1, [nondet]) :-
+    perform_action([on(a, b), clear(a), on(b, p1), clear(p2)], move(a, b, p2), X),
+    X == [on(a, p2), clear(b), clear(a), on(b, p1)].
+
+:- end_tests(perform_action).
