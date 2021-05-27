107005cd032fafc084a5c1e4a61e8b4ca7d33d77
mwiszenko <wiszenkom@gmail.com>
2021-05-27 14:15:22 +0200

    Add examples

--- src/plan.pl
+++ src/plan.pl
@@ -1,3 +1,21 @@
+% Pusta lista celów:
+% plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [], 5, A, B).
+
+% Wszystkie cele od razu osiągnięte:
+% plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [on(b3, b1), on(b4, p1), clear(b3)], 5, A, B).
+
+% Jeden cel do osiągnięcia:
+% plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [on(b4, b2)], 5, A, B).
+
+% Wiele celi do osiągnięcia:
+% plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [on(b4, b2), clear(p1), on(b2, b1), clear(p2), on(b1, p4)], 10, A, B).
+
+% Za mały limit długości planu:
+% plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [on(b4, b2), clear(p1), on(b2, b1), clear(p2), on(b1, p4)], 4, A, B).
+
+% Niemożliwy do zrealizowania:
+% plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [on(b4, b1), on(b1, b4)], 10, A, B).
+
 :- [utils, goals_achieved, requires, achieves, inst_action, perform_action, check_action, gen_limit, choose_goal].
 
 plan(InitState, Goals, MaxLimit, Plan, FinalState) :-
@@ -21,6 +39,3 @@ plan(InitState, Goals, AchievedGoals, Limit, Plan, FinalState) :-
     conc([Goal], AchievedGoals, AchievedGoals1),
     plan(State2, RestGoals, AchievedGoals1, LimitPost, PostPlan, FinalState),
     conc(PrePlan, [ InstAction | PostPlan ], Plan).
-
-ex_empty_goals() :-
-    plan([on(b3, b1), on(b1, b4), on(b4, p1), on(b2, p3), clear(b3), clear(p2), clear(b2), clear(p4)], [], 5, A, B).
